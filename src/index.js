20bb6bbb5c9d07a1e40d131347e79a69bd10e396
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-08-14 20:12:02 +0530

    VideoListItem

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyBWKQmo2izp7Hy3FqOjNg9SPq5MRKyoDgA';
 
-// Create a new component. This component should produce some HTML
-// const is ES16
-// Following is a JS function that returns JSX
-// JSX is not understood by browser
-// Hence it is transpiled to JS
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'HareKrishna'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+      return (
+        <div>
+          <SearchBar />
+          <VideoList videos={ this.state.videos } />
+        </div>
+    );
+  }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
